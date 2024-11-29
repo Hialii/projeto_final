6bf4ca51e73c1d68c34174317b3b25216838b173
hiali <hialidias@gmail.com>
2024-11-29 09:31:05 -0300

    feat: adicao da pagina Home

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,18 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+} from 'react-native';
 import { Link } from 'expo-router';
-
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/vagas">
-        Go to About screen
-      </Link>
+      <Text style={styles.text}>Veja as vagas disponíveis</Text>
+      <Link style={styles.link} href='./signIn'>Cadastrar</Link>
+      <Link  style={styles.link} href='./login'>Entrar</Link> 
     </View>
   );
 }
@@ -16,11 +20,25 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#f8f8f8',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: '#fff',
+    color: '#324e7b',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 32,
+    paddingHorizontal: 32,
+    textAlign: 'left',
   },
+  link: {
+    alignItems: 'center',
+    padding: 16,
+    borderRadius: 24,
+    borderWidth: 2,
+    margin: 6,
+    backgroundColor: '#324e7b',
+    color: '#fff',
+  }
 });
